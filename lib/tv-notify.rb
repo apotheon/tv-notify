fd6073a06e213bf66d8be983b2d9fafa2e797691
Bryan Dunsmore <dunsmoreb@gmail.com>
2012-05-30 14:44:20 -0500

    Add notifications for TV shows.
    Adds support for notifications for TV shows. Uses a config file to
    notify user about upcomming TV shows.

--- lib/tv-notify.rb
+++ lib/tv-notify.rb
@@ -3,9 +3,45 @@ require 'libnotify'
 require 'nokogiri'
 require 'net/http'
 require 'chronic'
+require 'yaml'
 require 'uri'
 
 # Require all providers.
 Dir[File.expand_path('tv-notify/*.rb', File.dirname(__FILE__))].each do |f|
   require f
+end
+
+# Get settings from config file.
+config_file = ARGV[0] || 'tv-notify.yml'
+settings = YAML.load_file(config_file)
+
+settings = settings.inject({}) do |memo, (k, v)|
+  memo[k.to_sym] = v
+  memo
+end
+
+# Get show listings.
+exit unless settings[:show]
+
+case settings[:provider]
+when 'locatetv'
+  listings = TVNotify::LocateTV.get_show_listings(settings[:show])
+
+else
+  puts 'Oh no!'
+  puts "Looks like you haven't set a provider."
+  exit
+end
+
+# Notify!
+time_limit = settings[:time] || 300 # = 5 Minutes
+
+listings.each do |listing|
+  show_time = Chronic.parse(listing[:time])
+  time_from_now = show_time - Time.now
+
+  if time_from_now <= time_limit and time_from_now >= 0
+    body = "#{listing[:time].capitalize} on #{listing[:channel]}"
+    Libnotify.show summary: settings[:show], body: body, timeout: 5, transient: true
+  end
 end
