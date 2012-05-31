6149e3837b8f999c3bdbe766f07440390b834a99
Bryan Dunsmore <dunsmoreb@gmail.com>
2012-05-30 20:20:43 -0500

    Remove unnecessary requires.
    Removes unncessary requires from `lib/tv-notify.rb`.

--- lib/tv-notify.rb
+++ lib/tv-notify.rb
@@ -1,10 +1,7 @@
 require 'bundler/setup'
 require 'libnotify'
-require 'nokogiri'
-require 'net/http'
 require 'chronic'
 require 'yaml'
-require 'uri'
 
 # Require all providers.
 Dir[File.expand_path('tv-notify/*.rb', File.dirname(__FILE__))].each do |f|
